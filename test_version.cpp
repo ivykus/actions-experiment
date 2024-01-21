3743cad49d7f80428272a58727fd86d83ae56162
vkudr <themoonissilent@gmail.com>
2024-01-21 18:21:53 +0300

    change test framework to gtest

--- test_version.cpp
+++ test_version.cpp
@@ -1,14 +1,7 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version)
+#include <gtest/gtest.h>
+TEST(test_version, test_valid_version)
 {
-    BOOST_CHECK(version() > 0);
+    EXPECT_GT(version(), 0);
+    EXPECT_EQ(1, 1);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
